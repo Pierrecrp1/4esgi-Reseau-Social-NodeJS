b77f6a7d8755b77741645a58eb4ad712fb3f633f
training-dev <contact@training-dev.fr>
2025-03-31 16:35:21 +0200

    nodejs with mongo db

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+const authMiddleware = require('./middleware/auth.middleware');
+
+const {connect} = require('./database/connection.js');
+
+//always return json objects
+app.use(express.json());
+
+const database = async () => {
+    await connect();
+}
+
+database();
+
+const productRoute = require("./routes/product.route");
+const authRoute = require("./routes/auth.route");
+
+//common headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', "GET, POST");
+
+    next();
+});
+
+
+app.use('/auth', authRoute);
+
+app.use('/product', authMiddleware, productRoute);
+
+module.exports = app;
